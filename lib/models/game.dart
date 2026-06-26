e911ea5c6ac6566ee276c72713489075889408cc
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-06-26 09:22:38 +0100

    projet de base

--- lib/models/game.dart
+++ lib/models/game.dart
@@ -1,10 +1,18 @@
 class Game {
   String bgImage;
-  String Icon;
+  String icon;
   String name;
   String type;
   num score;
   num download;
   num rewiew;
   String description;
+  List<String> images;
+
+  Game(
+    this.bgImage, 
+    this.icon, 
+    this.name, 
+    this.score
+  );
 }
