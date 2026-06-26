13521aad023342045c0aa87d2543a6231dad60af
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-06-26 09:42:54 +0100

    projet de base

--- lib/pages/home/widgets/popular.dart
+++ lib/pages/home/widgets/popular.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:game_app/models/game.dart';
 
 class PopularGame extends StatelessWidget {
-  const PopularGame({super.key});
+  PopularGame({super.key});
+
+  final List<Game> games = Game.games();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: ListView.separated(
+        itemBuilder: itemBuilder,
+        separatorBuilder: ((context, index) => SizedBox(width: 50)),
+        itemCount: games.length,
+      ),
+    );
   }
 }
